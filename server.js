981a4eaf25d4f4fd99983b7011ff3b319095ed25
jenyfferbarbara <haelton300988>
2021-05-16 01:27:14 -0300

    teste

--- server.js
+++ server.js
@@ -44,6 +44,56 @@ app.get("/install_requirements", (req, res, next) => {
 });
 
 app.get("/run_robot", async(req, res, next) => {
+
+	const user       = req.query.user
+	const wallet     = req.query.wallet
+	const stop_win   = req.query.stop_win
+	const stop_loss  = req.query.stop_loss
+	const expiration = req.query.expiration
+	const channel    = req.query.channel
+
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'],
+        scriptPath: 'iq_option',
+        args: [user, wallet, stop_win, stop_loss, expiration, channel]
+    };
+
+    const { success, err = '', results } = await new Promise(
+        (resolve, reject) =>
+        {
+			PythonShell.run('main.py', options,
+                function (err, results)
+                {
+                    if (err)
+                    {
+                        reject({ success: false, err });
+                    }
+
+                    console.log('PythonShell results: %j', results);
+
+                    resolve({ success: true, results });
+                }
+            );
+        }
+    );
+
+    console.log("python call ends");
+
+    if (! success)
+    {
+        console.log("Test Error: " + err);
+        return;
+    }
+
+    console.log("The result is: " + results);
+
+    // My code here
+
+    console.log("end runTest()");
+});
+
+app.get("/run_robot_old", async(req, res, next) => {
 	const user       = req.query.user
 	const wallet     = req.query.wallet
 	const stop_win   = req.query.stop_win
